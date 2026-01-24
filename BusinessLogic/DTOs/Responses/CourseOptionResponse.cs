6b162b2a3760367f88b647645896f08287b2da79
ino004 <haovo1417@gmail.com>
2026-01-24 12:16:02 +0700

    Grades

---
+++ BusinessLogic/DTOs/Responses/CourseOptionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessLogic.DTOs.Responses
+{
+    public class CourseOptionResponse
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; } = string.Empty;
+    }
+}
---
+++ BusinessLogic/Services/Interfaces/IGradeService.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogic.DTOs.Requests;
+using BusinessLogic.DTOs.Responses;
+
+namespace BusinessLogic.Services.Interfaces;
+
+public interface IGradeService
+{
+    Task<List<GradeResponse>> GetAllAsync();
+    Task<GradeResponse?> GetByIdAsync(int id);
+
+    // ✅ trả DTO đơn giản, không dùng SelectListItem
+    Task<List<EnrollmentOptionResponse>> GetEnrollmentOptionsAsync();
+
+    Task<int> CreateAsync(GradeUpsertRequest request);
+    Task<bool> UpdateAsync(int id, GradeUpsertRequest request);
+    Task<bool> DeleteAsync(int id);
+    Task<List<CourseOptionResponse>> GetCourseOptionsAsync();
+    Task<List<EnrollmentOptionResponse>> GetEnrollmentOptionsByCourseAsync(int courseId);
+
+}
